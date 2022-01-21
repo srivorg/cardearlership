31ab8eda4d4bc80b0b68197f8ab8cf67d927d792
sriv01 <58319384+sriv01@users.noreply.github.com>
2022-01-20 17:27:15 -0800

    Update Dealership.java

--- src/car/dealership/Dealership.java
+++ src/car/dealership/Dealership.java
@@ -1,14 +1,12 @@
 package car.dealership;
 
 public class Dealership {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Customer cust1 = new Customer();
 		cust1.setCustName("Test");
 		cust1.setCustAddress("abc");
 		cust1.setCashOnHand(1500);
-		
 		Vehicle vehicle = new Vehicle();
 		vehicle.setName("Tesla");
 		vehicle.setPrice(14999);
